dcd8c0d52acf658924872d83b8a4422cd29363f5
Zafreeti <jgabriel.basi@hotmail.com>
2017-04-04 17:06:10 -0300

    Adicionando um parser simples para json

--- main.gd
+++ main.gd
@@ -4,9 +4,21 @@ extends Panel
 const Face1 = preload("res://images/FirstCharFace.jpg")
 
 func _ready():
-	set_fixed_process(true)
 	get_node("FaceView1").set_texture(Face1)
+	var sceene = parse_sceene("test")
+	print(sceene[0]["Panel_1"])
+	print(sceene[1]["Panel_2"])
+	set_fixed_process(true)
 	pass
 	
+func parse_sceene(name):
+	var dict = {}
+	var file = File.new()
+	file.open("res://" + name + ".json", file.READ)
+	var text = file.get_as_text()
+	dict.parse_json(text)
+	file.close()
+	return dict[name]
+
 func _fixed_process(delta):
 	pass
