450584768fcb451015a50149cf0b863abf68d5f3
Zafreeti <jgabriel.basi@hotmail.com>
2017-04-28 21:34:59 -0300

    Adding dialogue1 and refactoring the code

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,27 +6,32 @@ var SCENES
 var ITENS
 var isTalking = 0
 var mouseInNPC2 = false
+var room
+var dialogue
 
 func _ready():
 	SCENES = parse_scene("scenes")
 	NPCS = parse_scene("NPCs")
 	ITENS = parse_scene("itens")
-	var room = SCENES["TestRoom"]
-	get_node("Background").set_texture(load(room["Background"]))
-	get_node("NPC1").set_normal_texture(getNPCImage(room, 0, "Body"))
-	get_node("NPC1").set_pos(getPos(room["Characters"][0]))
-	get_node("NPC2").set_normal_texture(getNPCImage(room, 1, "Body"))
-	get_node("NPC2").set_pos(getPos(room["Characters"][1]))
-	get_node("Item1").set_normal_texture(getItemImage(room, 0, "Image"))
-	get_node("Item1").set_pos(getPos(room["Itens"][0]))
+	loadScene("TestRoom")
 	set_fixed_process(true)
 	set_process_input(true)
 	pass
+	
+func loadScene(name):
+	room = SCENES[name]
+	get_node("Background").set_texture(load(room["Background"]))
+	for i in range(room["Characters"].size()):
+		get_node("NPC"+str(i+1)).set_normal_texture(getNPCImage(i, "Body"))
+		get_node("NPC"+str(i+1)).set_pos(getPos(room["Characters"][i]))
+	for i in range(room["Itens"].size()):
+		get_node("Item"+str(i+1)).set_normal_texture(getItemImage(i, "Image"))
+		get_node("Item"+str(i+1)).set_pos(getPos(room["Itens"][i]))
 
-func getNPCImage(room, num, type):
+func getNPCImage(num, type):
 	return load(NPCS[room["Characters"][num][0]][type])
 	
-func getItemImage(room, num, type):
+func getItemImage(num, type):
 	return load(ITENS[room["Itens"][num][0]][type])
 
 func getPos(vec):
@@ -44,13 +49,14 @@ func parse_scene(name):
 func _fixed_process(delta):
 	pass
 
-func _input(event):
-	verify_click(event)
+func _on_NPC1_pressed():
+	print("Entrow")
+	get_node("FaceView1").set_texture(getNPCImage(0, "Face"))
+	get_node("FaceView1").get_node("appear").play("face_appear")
+	get_node("NPC1").set_opacity(0)
+	get_node("DarkLight").get_node("dim").play("make_it_dim")
 	pass
 
-func verify_click(event):
-	if (event.is_action_pressed("click_on_character") and mouseInNPC2):
-		get_node("FaceView1").get_node("appear").play("face_appear")
-		get_node("NPC2").set_opacity(0)
-		get_node("Polygon2D").get_node("dim").play("make_it_dim")
+func _on_NPC2_pressed():
+	print("Entrow")
 	pass
